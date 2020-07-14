718958b32d84e7b099e10b0c25520e21812b0f89
Frics <jimmy9711@naver.com>
2020-07-15 01:06:19 +0900

    Solve N1011 By C++

--- BaekJoon_C++/N1011/main.cpp
+++ BaekJoon_C++/N1011/main.cpp
@@ -1,47 +1,36 @@
-//
-//  main.cpp
-//  N1011
-//
-//  Created by James Choi on 2020/07/14.
-//  Copyright © 2020 James Choi. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
 
-int getCount(int x, int y){
-    
-    if(y-x ==1)
-        return 1;
-    else if(y-x ==2)
-        return 2;
-    else{
-        int cnt = 2, i;
-        int len = y-x-2;
-        int mid;
-        if(len/2 >0)
-            mid = len/2+1;
-        else
-            mid= 1;
-        for(i=1;mid>0; i++){
-            mid-=i;
+int getAlpha(long long x, long long y){
+    long long distance = y- x;
+    long long front = 1, back = 1;
+    long long cnt = 0;
+    while(front < distance){
+        cnt++;
+        back++;
+        front += cnt;
+        if(front >= distance)
+            break;
+        back++;
+        front += cnt;
+        if(front >= distance)
+            break;
         }
-        if(mid==0)
-            cnt += i*2;
-        if(mid<0)
-            cnt += i*2-1;
-            
-        
-        return cnt;
-    }
-    
+        if(front > distance)
+            back--;
+        return back;
 }
 int main() {
-    int T, x, y;
+    int T;
+    long long x, y;
+    
     cin >> T;
     
     for(int i=0; i<T; i++){
-        cin >> x >> y;
-        cout << getCount(x, y);
+        cin >>  x >> y;
+        
+        long long result = getAlpha(x, y);
+        cout << result << "\n";
+        
     }
 }
