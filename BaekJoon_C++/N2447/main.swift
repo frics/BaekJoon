c91f183979f24c132ef94d2658b174bdbe853991
Frics <jimmy9711@naver.com>
2020-07-21 16:06:47 +0900

    Solve N2447 by C++

---
+++ BaekJoon_C++/N2447/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  N2447
+//
+//  Created by James Choi on 2020/07/21.
+//  Copyright © 2020 James Choi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
