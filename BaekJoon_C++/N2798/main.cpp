6d444c9d14d0b9b08bc209ed6d32aac02df53b91
Frics <jimmy9711@naver.com>
2020-07-22 13:16:51 +0900

    Solve N2798 by C++

--- BaekJoon_C++/N2798/main.cpp
+++ BaekJoon_C++/N2798/main.cpp
@@ -9,19 +9,31 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+int blackjack(int N, int M, int *card){
+    int max =0;
+    
+    for(int i=0; i<N-2; i++){
+        for(int j=i+1; j<N; j++){
+            for(int k=j+1; k<N; k++){
+                int tmp = card[i]+card[j]+card[k];
+                if(max<tmp && tmp <=M){
+                    max= tmp;
+                }
+            }
+        }
+    }
+    return max;
+}
+int main() {
     int N, M;
     int card[100] = {0,};
-    int max =0;
+    
     cin >> N >> M;
     
     for(int i=0; i<N; i++){
         cin >> card[i];
     }
-    
-    for(int i=0; i<N; i++){
-        for(int j=i; j<N; j++){
-            if(max-)
-        }
-    }
+    int max;
+    max = blackjack(N, M, card);
+    printf("%d\n", max);
 }
