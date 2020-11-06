671a682f92ba2ad89a3e02d621038cb213693288
Frics <jimmy9711@naver.com>
2020-11-07 01:52:41 +0900

    tmp backup:

--- BaekJoon_C++/N14888/main.cpp
+++ BaekJoon_C++/N14888/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+using namespace std;
+
+struct Operator{
+    char symbol;
+    int cnt;
+};
+
+Operator op[4];
+int num[11] = {0, };
+
+bool isPromising(){//max값이나 min값이 갱신이 되야 해당 노드 방문하게
+    
+    return true;
+}
+
+void getMinMax(int N){
+    
+    for(int i=0; i<N; i++){
+        if(
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int N;
+    
+    op[0].symbol = '+';
+    op[1].symbol = '-';
+    op[2].symbol = '*';
+    op[3].symbol = '/';
+    
+    cin >> N;
+    for(int i=0; i<N; i++)
+        cin >> num[i];
+    for(int i=0; i<4; i++)
+        cin >> op[i].cnt;
+    
+    for(int i=0; i<4; i++)
+        cout << op[i].symbol << " "<<op[i].cnt<< "\n";
+        
+    
+    
 }
